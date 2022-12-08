442bde4f77c958c6146920869789a591aaa36a5c
Alessandro Modolo <alessandro.modolo@overit.it>
2022-12-08 21:48:45 +0100

    fix spaces

--- src/main/java/com/example/playground/ActionsPlaygroundApplication.java
+++ src/main/java/com/example/playground/ActionsPlaygroundApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ActionsPlaygroundApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ActionsPlaygroundApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ActionsPlaygroundApplication.class, args);
+    }
 
 }
